45bf69c89a89e988fce429387cd055406e60040d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-02 22:23:26 -0300

    Configuração do @material-ui/pickers

--- src/app/App.js
+++ src/app/App.js
@@ -2,18 +2,23 @@ import React from 'react'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './config/theme'
 import { Typography } from '@material-ui/core'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className='App'>
-        <Typography variant='h3' component='h1' gutterBottom>
-          Olá mundo!
-        </Typography>
-        <Typography variante='p' component='p' gutterBottom>
-          Demo
-        </Typography>
-      </div>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ptBR}>
+        <div className='App'>
+          <Typography variant='h3' component='h1' gutterBottom>
+            Olá mundo!
+          </Typography>
+          <Typography variante='p' component='p' gutterBottom>
+            Demo
+          </Typography>
+        </div>
+      </MuiPickersUtilsProvider>
     </ThemeProvider>
   )
 }
