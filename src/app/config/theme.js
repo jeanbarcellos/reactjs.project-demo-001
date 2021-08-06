aa53e33c5777e2e2d9e958ff5dd7d9f30cae462b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-05 21:19:38 -0300

    Material-UI - Configuração da tipografia do theme

--- src/app/config/theme.js
+++ src/app/config/theme.js
@@ -7,6 +7,19 @@ const theme = createTheme({
   },
   status: {
     danger: orange[500]
+  },
+  typography: {
+    htmlFontSize: 10,
+    fontSize: 13,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    body1: {
+      fontSize: '1.3rem'
+    },
+    body2: {
+      fontSize: '1.3rem'
+    }
   }
 })
 
