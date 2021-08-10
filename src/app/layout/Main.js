a69d54c7a09d675d495bb8d216d167f2edadd570
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-09 21:51:08 -0300

    Criação de Pages

--- src/app/layout/Main.js
+++ src/app/layout/Main.js
@@ -1,54 +1,8 @@
+import DashboardPage from 'app/pages/DashboardPage'
 import React from 'react'
 
 const Main = () => {
-  return (
-    <div className='p-24'>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit turpis sed metus ullamcorper sagittis.
-        Maecenas aliquet finibus placerat. Nunc tristique at purus vel porta. Pellentesque nec erat id purus mattis
-        posuere sed id mauris. Cras mauris elit, gravida quis gravida ac, ornare vel urna. Vestibulum sed est eu dolor
-        ullamcorper varius. In euismod libero sit amet sem lacinia malesuada. Donec mattis est auctor mauris mollis
-        tempor. Praesent rhoncus eros eget diam pharetra, nec fringilla odio rutrum. Suspendisse lacinia, eros et
-        rhoncus mattis, diam eros maximus eros, vitae molestie nisl quam in odio.
-      </p>
-      <p>
-        Suspendisse a diam ac nulla congue sagittis. Sed libero tellus, blandit eu eleifend eget, vehicula a risus.
-        Suspendisse molestie blandit placerat. Nullam in urna sem. Vivamus dignissim felis eros, vel volutpat lacus
-        rutrum vitae. Aliquam sed lacus lobortis, eleifend dui sit amet, laoreet orci. Vivamus pharetra ante ac libero
-        rutrum, eget egestas dolor porttitor. Curabitur velit leo, fringilla a eros ac, malesuada suscipit nibh. Donec
-        id lectus id ipsum porta pellentesque a quis ex. Cras quis accumsan nunc. Vestibulum id eros eu risus suscipit
-        imperdiet. Vestibulum lectus nunc, scelerisque vel libero auctor, feugiat placerat lectus.
-      </p>
-      <p>
-        Suspendisse potenti. Aenean ut lacus semper, dapibus sapien vel, interdum mauris. Aenean ultricies augue at
-        blandit lacinia. Vivamus tincidunt mi justo, in pellentesque nulla tempus hendrerit. Duis vestibulum ac nibh vel
-        laoreet. Fusce sodales enim id dolor varius, non pharetra lacus porttitor. Duis ornare massa sed fermentum
-        pellentesque. Vestibulum cursus nibh augue. Proin vel tincidunt libero. Mauris a magna ac magna maximus
-        molestie. Integer a varius augue. Nam accumsan diam a eros vehicula scelerisque. Aenean a aliquet ligula. Fusce
-        a dictum dui, vel eleifend dolor.
-      </p>
-      <p>
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque vitae ligula sit amet risus tristique
-        elementum. Nulla quis sapien non ipsum pretium rutrum efficitur eu felis. Phasellus quis aliquam tortor. Vivamus
-        magna nulla, luctus in consectetur tincidunt, suscipit sagittis magna. Proin nunc enim, ultricies finibus
-        tristique vitae, bibendum mollis tortor. Sed a lorem in velit mollis sodales id ut lectus. Integer ac tempus
-        lectus. Donec laoreet ornare condimentum. Praesent non purus luctus, suscipit erat eu, tempus ipsum. Donec quam
-        ante, aliquam quis dictum ac, porta placerat quam. Suspendisse nisi magna, interdum et mauris id, accumsan
-        gravida velit. Curabitur ac sem diam. Vestibulum eu quam ac enim volutpat tempor vitae vel quam. Morbi ex ex,
-        hendrerit sed justo ac, tincidunt tristique lectus.
-      </p>
-      <p>
-        Integer placerat, purus placerat euismod malesuada, enim elit placerat lectus, ac porttitor sapien lectus vitae
-        dolor. Mauris vel purus eu est vulputate eleifend non et nulla. Donec congue fringilla purus, vel convallis est
-        tempor in. Curabitur mi nulla, tristique a odio et, malesuada tempor lacus. Aenean tincidunt ex nunc, non
-        pharetra ligula auctor in. Donec euismod tristique velit. Mauris in arcu eleifend, lacinia urna et, ultrices
-        massa. Nunc iaculis nisl erat, sed lacinia odio pretium nec. Phasellus elit tortor, mattis id tincidunt ut,
-        facilisis at purus. Praesent in efficitur diam. Sed sollicitudin tortor non mollis interdum. Donec tempor eros
-        ut libero vulputate, at dignissim sem luctus. Sed mattis quam at odio malesuada, id consectetur arcu iaculis.
-        Vivamus dignissim diam at mi fermentum iaculis. Curabitur tristique libero vitae dui vestibulum egestas.
-      </p>
-    </div>
-  )
+  return <DashboardPage />
 }
 
 export default Main
