29a2d2a2f2e9c11afbe6c3946280e0b51efa76e2
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-09 22:22:52 -0300

    App/Pages - Criação da página de erro

---
+++ src/app/pages/Error404Page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+const Error404Page = () => {
+  return (
+    <div className='p-24 text-center'>
+      <Typography variant='h3' component='h3' className='mb-24'>
+        Error 404
+      </Typography>
+      <p className='mb-16'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit turpis sed metus ullamcorper sagittis.
+        Maecenas aliquet finibus placerat. Nunc tristique at purus vel porta. Pellentesque nec erat id purus mattis
+        posuere sed id mauris. Cras mauris elit, gravida quis gravida ac, ornare vel urna. Vestibulum sed est eu dolor
+        ullamcorper varius. In euismod libero sit amet sem lacinia malesuada. Donec mattis est auctor mauris mollis
+        tempor. Praesent rhoncus eros eget diam pharetra, nec fringilla odio rutrum. Suspendisse lacinia, eros et
+        rhoncus mattis, diam eros maximus eros, vitae molestie nisl quam in odio.
+      </p>
+    </div>
+  )
+}
+
+export default Error404Page
