04bf6ab8b9df3851d31307028bda86f5c5f840f6
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-10 21:23:55 -0300

    App\Pages - Reorganização

--- src/app/config/routesConfig.js
+++ src/app/config/routesConfig.js
@@ -9,32 +9,32 @@ const routesConfig = [
   },
   {
     path: '/dashboard',
-    component: React.lazy(() => import('app/pages/DashboardPage')),
+    component: React.lazy(() => import('app/pages/dashboard/DashboardPage')),
     exact: true
   },
   {
     path: '/orders',
-    component: React.lazy(() => import('app/pages/OrdersPage')),
+    component: React.lazy(() => import('app/pages/orders/OrdersPage')),
     exact: true
   },
   {
     path: '/customers',
-    component: React.lazy(() => import('app/pages/CustomersPage')),
+    component: React.lazy(() => import('app/pages/customers/CustomersPage')),
     exact: true
   },
   {
     path: '/reports',
-    component: React.lazy(() => import('app/pages/ReportsPage')),
+    component: React.lazy(() => import('app/pages/reports/ReportsPage')),
     exact: true
   },
   {
     path: '/integrations',
-    component: React.lazy(() => import('app/pages/IntegrationsPage')),
+    component: React.lazy(() => import('app/pages/integrations/IntegrationsPage')),
     exact: true
   },
   {
     path: '/error/404',
-    component: React.lazy(() => import('app/pages/Error404Page')),
+    component: React.lazy(() => import('app/pages/errors/Error404Page')),
     exact: true
   },
   {
