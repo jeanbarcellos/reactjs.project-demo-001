dac2960812f291ac048ade9c6bcff0a02a919bec
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-12 22:52:06 -0300

    App - Criação de fake-db

---
+++ src/app/@fake-bd/categoriesDb.js
@@ -0,0 +1,42 @@
+const categoriesDb = {
+  categories: [
+    {
+      id: 1,
+      name: 'Family',
+      createdAt: '2021-08-12T21:45:15Z',
+      updatedAt: '2021-08-12T21:45:15Z'
+    },
+    {
+      id: 2,
+      name: 'Work',
+      createdAt: '2021-08-12T21:45:15Z',
+      updatedAt: '2021-08-12T21:45:15Z'
+    },
+    {
+      id: 3,
+      name: 'Todos',
+      createdAt: '2021-08-12T21:45:15Z',
+      updatedAt: '2021-08-12T21:45:15Z'
+    },
+    {
+      id: 4,
+      name: 'Prior',
+      createdAt: '2021-08-12T21:45:15Z',
+      updatedAt: '2021-08-12T21:45:15Z'
+    },
+    {
+      id: 5,
+      name: 'Personal',
+      createdAt: '2021-08-12T21:45:15Z',
+      updatedAt: '2021-08-12T21:45:15Z'
+    },
+    {
+      id: 6,
+      name: 'Friends',
+      createdAt: '2021-08-12T21:45:15Z',
+      updatedAt: '2021-08-12T21:45:15Z'
+    }
+  ]
+}
+
+export default categoriesDb
