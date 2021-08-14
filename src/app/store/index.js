f4180cc3b4d214aa33835d46c53a921b0b6aae0e
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-14 19:22:55 -0300

    store - ajustes

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -3,6 +3,8 @@ import rootReducers from './rootReducer'
 
 const store = configureStore({
   reducer: rootReducers,
+  middleware: [],
+  enhancers: [],
   devTools: process.env.NODE_ENV === 'development'
 })
 
