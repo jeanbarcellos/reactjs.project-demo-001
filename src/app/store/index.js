81036461ff67ca4c8ca946bdeb9b75753f76f30f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-14 21:15:44 -0300

    store - melhorias em injectReducer

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -3,7 +3,6 @@ import appReducers from './app'
 import logger from './middlewares/logger'
 import monitorReducerEnhancer from './enhancers/monitorReducer'
 
-// Defina os Redutores que sempre estarão presentes na aplicação
 const staticReducers = {
   app: appReducers
 }
@@ -26,15 +25,15 @@ const store = configureStore({
   devTools: process.env.NODE_ENV === 'development'
 })
 
-// Adicione um dicionário para acompanhar os redutores assíncronos registrados
 store.asyncReducers = {}
 
-// Esta função adiciona o redutor assíncrono e cria um novo redutor combinado
 export const injectReducer = (key, asyncReducer) => {
+  if (store.asyncReducers[key]) {
+    return false
+  }
+
   store.asyncReducers[key] = asyncReducer
   store.replaceReducer(createReducer(store.asyncReducers))
-
-  // Devolver a loja modificada
   return store
 }
 
