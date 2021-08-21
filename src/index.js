3df55ff224aa9efb7e16da47890f1955fcd1bf5b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-21 14:40:51 -0300

    config - Adição de configuração de importação de estilos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/app.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import './config/styleConfig'
 
 ReactDOM.render(
   <React.StrictMode>
