a5817718a8f2dea1a3095100d74cbfbb04289539
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-25 00:15:56 -0300

    Pages/Products - Obtenção e Exibição da lista de produtos

--- src/pages/products/ProductsPage.js
+++ src/pages/products/ProductsPage.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PageTile from 'components/page/PageTile'
-import PageContentDemo from 'components/page/PageContentDemo'
 import withReducer from 'store/withReducer'
 import reducers from './store'
 import config from './config'
+import { useDispatch, useSelector } from 'react-redux'
+import { resetProducts, getProducts, selectAllProducts } from './store/productsSlice'
 
 const ProductsPage = () => {
+  const dispatch = useDispatch()
+
+  const products = useSelector(selectAllProducts)
+
+  useEffect(() => {
+    dispatch(resetProducts())
+    dispatch(getProducts())
+  }, [dispatch])
+
   return (
     <div className='p-24 text-justify'>
       <PageTile>Products</PageTile>
-      <PageContentDemo />
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </div>
   )
 }
