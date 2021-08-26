6f11d6594533b939b01df0f8115d22f48fcb7baf
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-25 22:16:39 -0300

    Implementação da listagem de produtos

--- src/pages/products/ProductsPage.js
+++ src/pages/products/ProductsPage.js
@@ -1,25 +1,109 @@
 import React, { useEffect } from 'react'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 import PageTile from 'components/page/PageTile'
 import withReducer from 'store/withReducer'
 import reducers from './store'
 import config from './config'
 import { useDispatch, useSelector } from 'react-redux'
 import { resetProducts, getProducts, selectAllProducts } from './store/productsSlice'
+import OrderedTableHead from 'components/table/OrderedTableHead'
+import useTable from 'hooks/useTable'
+import { toStringDateTime } from 'utils/date'
+
+const header = [
+  { id: 'id', label: 'ID' },
+  { id: 'categoryId', label: 'Category' },
+  { id: 'name', label: 'Name' },
+  { id: 'description', label: 'Description' },
+  { id: 'active', label: 'active' },
+  { id: 'quantity', label: 'Quantity' },
+  { id: 'value', label: 'Value' },
+  { id: 'createdAt', label: 'Created At' },
+  { id: 'updatedAt', label: 'Updated At' },
+  { id: 'actions', label: '', sort: false }
+]
 
 const ProductsPage = () => {
   const dispatch = useDispatch()
 
   const products = useSelector(selectAllProducts)
 
+  const { order, handleRequestSort, getFilteredData } = useTable(0, 5, 'name')
+
   useEffect(() => {
     dispatch(resetProducts())
     dispatch(getProducts())
   }, [dispatch])
 
+  const orderIteratee = o => {
+    switch (order.id) {
+      case 'id': {
+        return parseInt(o.id, 10)
+      }
+      case 'categoryId': {
+        return o.categoryId
+      }
+      case 'name': {
+        return o.name
+      }
+      case 'description': {
+        return o.description
+      }
+      case 'active': {
+        return o.active
+      }
+      case 'quantity': {
+        return o.quantity
+      }
+      case 'value': {
+        return o.value
+      }
+      case 'createdAt': {
+        return o.createdAt
+      }
+      case 'updatedAt': {
+        return o.updatedAt
+      }
+      default: {
+        return o[order.id]
+      }
+    }
+  }
+
   return (
     <div className='p-24 text-justify'>
       <PageTile>Products</PageTile>
-      <pre>{JSON.stringify(products, null, 2)}</pre>
+
+      <Paper>
+        <TableContainer>
+          <Table size='medium'>
+            <OrderedTableHead data={header} order={order} onRequestSort={handleRequestSort} />
+            <TableBody>
+              {getFilteredData(products, orderIteratee).map(row => {
+                return (
+                  <TableRow hover key={row.id}>
+                    <TableCell align='left'>{row.id}</TableCell>
+                    <TableCell align='left'>{row.categoryId}</TableCell>
+                    <TableCell align='left'>{row.name}</TableCell>
+                    <TableCell align='left'>{row.description}</TableCell>
+                    <TableCell align='left'>{row.active ? 'Active' : 'Inactive'}</TableCell>
+                    <TableCell align='left'>{row.quantity}</TableCell>
+                    <TableCell align='left'>{row.value}</TableCell>
+                    <TableCell align='left'>{toStringDateTime(row.createdAt)}</TableCell>
+                    <TableCell align='left'>{toStringDateTime(row.updatedAt)}</TableCell>
+                    <TableCell align='left'>-</TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   )
 }
