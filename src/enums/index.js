501405cd80a680e983375bab12b90be82accdecc
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-17 19:36:03 -0300

    Criação de enuns

---
+++ src/enums/index.js
@@ -0,0 +1,17 @@
+export const DateFormatEnum = {
+  DATE: 'dd/MM/yyyy',
+  TIME: 'HH:mm:ss',
+  DATETIME: 'dd/MM/yyyy HH:mm',
+  DATETIME_WITH_SECS: 'dd/MM/yyyy HH:mm:ss'
+}
+
+export const ScenarioTypeEnum = {
+  CREATE: 'new',
+  EDIT: 'edit',
+  REMOVE: 'remove'
+}
+
+export const SortEnum = {
+  ASCENDING: 'asc',
+  DESCENDING: 'desc'
+}
