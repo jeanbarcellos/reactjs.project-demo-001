2e727c9fd5af772991be4419414679ebaa498b17
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-17 16:02:07 -0300

    config - ajustes

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+import apiConfig from './apiConfig'
+import appConfig from './appConfig'
+import layoutConfig from './layoutConfig'
+import themeConfig from './themeConfig'
+
+const config = {
+  app: appConfig,
+  api: apiConfig,
+  layout: layoutConfig,
+  theme: themeConfig
+}
+
+export default config
