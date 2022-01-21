b39e7d991217bb6e6b01d5f8c56a859eb06b7f64
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-20 23:38:59 -0300

    Troca do @material-ui/core por @mui/material em App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import React from 'react'
-import { ThemeProvider } from '@material-ui/styles'
+import { ThemeProvider } from '@mui/material'
 import themeConfig from 'config/themeConfig'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-import DateFnsUtils from '@date-io/date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import { create } from 'jss'
-import { StylesProvider, jssPreset } from '@material-ui/core/styles'
-import Layout from './layout'
-import { BrowserRouter as Router } from 'react-router-dom'
-import store from './store'
+import React from 'react'
 import { Provider } from 'react-redux'
-
-const jss = create({
-  ...jssPreset(),
-  insertionPoint: document.getElementById('jss-insertion-point')
-})
+import { BrowserRouter as Router } from 'react-router-dom'
+import Layout from './layout'
+import store from './store'
 
 const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={themeConfig}>
-        <StylesProvider jss={jss}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ptBR}>
-            <Router>
-              <Layout />
-            </Router>
-          </MuiPickersUtilsProvider>
-        </StylesProvider>
+        <Router>
+          <Layout />
+        </Router>
       </ThemeProvider>
     </Provider>
   )
