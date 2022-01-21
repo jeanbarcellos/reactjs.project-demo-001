d490a033b69d1c8461d1bce1e7929b7a076be7f8
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-21 01:59:45 -0300

    Configuração do tss-react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
 import { ThemeProvider } from '@mui/material'
 import themeConfig from 'config/themeConfig'
 import React from 'react'
@@ -6,14 +8,21 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Layout from './layouts'
 import store from './store'
 
+export const muiCache = createCache({
+  key: 'mui',
+  prepend: true
+})
+
 const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={themeConfig}>
-        <Router>
-          <Layout />
-        </Router>
-      </ThemeProvider>
+      <CacheProvider value={muiCache}>
+        <ThemeProvider theme={themeConfig}>
+          <Router>
+            <Layout />
+          </Router>
+        </ThemeProvider>
+      </CacheProvider>
     </Provider>
   )
 }
