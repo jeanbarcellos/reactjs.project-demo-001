2ea8fb374f4547dc3269182271b8263eeef97bd3
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-23 01:20:13 -0300

    config - adição da localização no theme

--- src/config/themeConfig.js
+++ src/config/themeConfig.js
@@ -1,26 +1,30 @@
 import { createTheme } from '@mui/material/styles'
 import { orange } from '@mui/material/colors'
+import { ptBR } from '@mui/material/locale'
 
-const themeConfig = createTheme({
-  palette: {
-    type: 'light'
-  },
-  status: {
-    danger: orange[500]
-  },
-  typography: {
-    htmlFontSize: 10,
-    fontSize: 13,
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    body1: {
-      fontSize: '1.3rem'
+const themeConfig = createTheme(
+  {
+    palette: {
+      type: 'light'
     },
-    body2: {
-      fontSize: '1.3rem'
+    status: {
+      danger: orange[500]
+    },
+    typography: {
+      htmlFontSize: 10,
+      fontSize: 13,
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      body1: {
+        fontSize: '1.3rem'
+      },
+      body2: {
+        fontSize: '1.3rem'
+      }
     }
-  }
-})
+  },
+  ptBR
+)
 
 export default themeConfig
