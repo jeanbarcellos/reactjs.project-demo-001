2a68c625058566e955af1e8f29ddd8733a674dd6
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-25 08:29:11 -0300

    store - criação do slice auth para usuário logado

---
+++ src/store/app/auth/userSlice.js
@@ -0,0 +1,39 @@
+const { createSlice } = require('@reduxjs/toolkit')
+
+const reducerName = `app/auth/user`
+
+// Initial State
+
+const initialState = {
+  data: null
+  // data: {
+  //   id: null,
+  //   displayName: 'Anônimo'
+  // }
+}
+
+// Slice
+
+const userSlice = createSlice({
+  name: reducerName,
+  initialState,
+  reducers: {
+    resetUser: (state, action) => initialState,
+    setUser: (state, action) => {
+      state.data = action.payload
+    }
+  },
+  extraReducers: {}
+})
+
+// Actions
+
+export const { resetUser, setUser } = userSlice.actions
+
+// Selectors
+
+export const selectUser = state => state.app.auth.user
+
+// Reducer
+
+export default userSlice.reducer
--- src/store/app/index.js
+++ src/store/app/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from '@reduxjs/toolkit'
 import navbarSlice from './navbarSlice'
 import dialogSlice from './dialogSlice'
 import messageSlice from './messageSlice'
+import auth from './auth'
 
 // Aqui serão adicionados os reducers referentes ao sistema em geral
 const appReducers = combineReducers({
   navbar: navbarSlice,
   dialog: dialogSlice,
-  message: messageSlice
+  message: messageSlice,
+  auth
 })
 
 export default appReducers
