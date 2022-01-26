cef448e613088fb62a7b2fb2473d2a11c02b9a00
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-26 12:00:35 -0300

    config - configuração de rotas

--- src/config/routesConfig.js
+++ src/config/routesConfig.js
@@ -1,32 +1,16 @@
+import { indexRoute as dashboardIndexRoute } from 'modules/dashboard/routes'
+import { error404Route } from 'modules/errors/routes'
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-
-import { default as authRoutes } from 'modules/auth/routes'
-import { default as dashboardRoutes, indexRoute as dashboardIndexRoute } from 'modules/dashboard/routes'
-import { default as productsRoutes } from 'modules/products/routes'
-import { default as categoryRoutes } from 'modules/categories/routes'
-import { default as ordersRoutes } from 'modules/orders/routes'
-import { default as customersRoutes } from 'modules/customers/routes'
-import { default as reportsRoutes } from 'modules/reports/routes'
-import { default as examplesRoutes } from 'modules/examples/routes'
-import { default as counterRoutes } from 'modules/counter/routes'
-import { default as errorsRoutes, error404Route } from 'modules/errors/routes'
+import { generateRoutesFromModuleConfig } from 'utils'
+import modulesConfig from './modulesConfig'
 
 const routesConfig = [
   {
     path: '/',
     element: <Navigate to={dashboardIndexRoute()} />
   },
-  ...authRoutes,
-  ...dashboardRoutes,
-  ...categoryRoutes,
-  ...productsRoutes,
-  ...ordersRoutes,
-  ...customersRoutes,
-  ...reportsRoutes,
-  ...examplesRoutes,
-  ...counterRoutes,
-  ...errorsRoutes,
+  ...generateRoutesFromModuleConfig(modulesConfig),
   {
     path: '*',
     element: <Navigate to={error404Route()} />
