f11a3eec7f46013cfd6d0ff98a983d5854c7a756
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-26 11:59:55 -0300

    utils - criação de utilitários para routas

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,27 @@
 export { default } from './Utils'
+
+export const generateRoutesFromModuleConfig = configs => {
+  let routes = []
+
+  configs.forEach(config => {
+    routes = [...routes, ...generateRouteFromModuleConfig(config)]
+    console.log(config.routes)
+  })
+
+  return routes
+}
+
+const generateRouteFromModuleConfig = config => {
+  let routes = [...config.routes]
+
+  routes = routes.map(route => {
+    const layout = { ...config.layout, ...route.layout }
+
+    return {
+      ...route,
+      layout
+    }
+  })
+
+  return routes
+}
