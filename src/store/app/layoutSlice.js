fe6265a2bc902e08e3c5f4d34f275f245ac7f875
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-28 20:33:59 -0300

    store - ajustes no slice layout

--- src/store/app/layoutSlice.js
+++ src/store/app/layoutSlice.js
@@ -1,20 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit'
 import layoutConfig from 'config/layoutConfig'
+import _ from 'lodash'
 
 const reducerKey = `layout`
 const reducerName = `app/${reducerKey}`
 
+// Initial state
+
 const initialState = {
-  ...layoutConfig
+  default: { ...layoutConfig },
+  current: { ...layoutConfig }
 }
+
+// Slice
 
 const layoutSlice = createSlice({
   name: reducerName,
   initialState,
-  reducers: {},
+  reducers: {
+    setLayout: (state, action) => {
+      const current = generateLayout(action.payload)
+      return {
+        ...state,
+        current
+      }
+    }
+  },
   extraReducers: {}
 })
 
-export const selectLayoutConfig = state => state.app[reducerKey]
+// actions
+
+export const { setLayout } = layoutSlice.actions
+
+// Selectors
+
+export const selectLayoutDefaultConfig = state => state.app[reducerKey].default
+
+export const selectLayoutConfig = state => state.app[reducerKey].current
+
+// functions
+
+export const generateLayout = newLayout => {
+  return _.merge({}, layoutConfig, newLayout)
+}
 
 export default layoutSlice.reducer
