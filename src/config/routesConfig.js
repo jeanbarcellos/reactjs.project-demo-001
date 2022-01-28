1e51993862400ecdbb961f6ae71f94f99dc3c96a
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-28 20:35:40 -0300

    config - melhorias em routesConfig

--- src/config/routesConfig.js
+++ src/config/routesConfig.js
@@ -2,15 +2,20 @@ import { indexRoute as dashboardIndexRoute } from 'modules/dashboard/routes'
 import { error404Route } from 'modules/errors/routes'
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-import { generateRoutesFromModuleConfig } from 'utils'
+import { generateRoutesFromModuleConfig } from 'utils/route'
 import modulesConfig from './modulesConfig'
+
+const modulesRoutes = generateRoutesFromModuleConfig(modulesConfig)
+
+console.log('modulesRoutes')
+console.table(modulesRoutes)
 
 const routesConfig = [
   {
     path: '/',
     element: <Navigate to={dashboardIndexRoute()} />
   },
-  ...generateRoutesFromModuleConfig(modulesConfig),
+  ...modulesRoutes,
   {
     path: '*',
     element: <Navigate to={error404Route()} />
