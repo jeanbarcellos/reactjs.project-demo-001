5f50ed7e75500c0883ecbfa4d73d455ad60ab550
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-28 09:34:49 -0300

    utils - Criação do EventEmitter

---
+++ src/utils/EventEmitter.js
@@ -0,0 +1,31 @@
+// Link: https://betterprogramming.pub/how-to-create-your-own-event-emitter-in-javascript-fbd5db2447c4
+
+class EventEmitter {
+  constructor() {
+    this.events = {}
+  }
+
+  _getEventsByName(eventName) {
+    if (typeof this.events[eventName] === 'undefined') {
+      this.events[eventName] = new Set()
+    }
+    return this.events[eventName]
+  }
+
+  on(eventName, fn) {
+    this._getEventsByName(eventName).add(fn)
+  }
+
+  removeListener(eventName, fn) {
+    this._getEventsByName(eventName).delete(fn)
+  }
+
+  emit(eventName, ...args) {
+    this._getEventsByName(eventName).forEach(
+      // eslint-disable-next-line func-names
+      function (fn) {
+        fn.apply(this, args)
+      }.bind(this)
+    )
+  }
+}
