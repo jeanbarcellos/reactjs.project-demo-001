f0e7ea59de5a63dcb856cf312b6f2f8eb72f4fd1
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-28 18:24:42 -0300

    services/auth - centralizando as Roles

---
+++ src/services/auth/Roles.js
@@ -0,0 +1,12 @@
+// ATENÇÂO: As roles devem ser as mesmas definidas no backend
+
+export const Roles = {
+  // Somente desenvolvedor ou dono do software
+  ROOT: 'root',
+
+  // Geral
+  ADMIN: 'admin',
+  DEFAULT: 'default'
+}
+
+export default Roles
