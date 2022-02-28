8d803046840e1d8143227224bfae1bf2df727d7b
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-28 18:24:23 -0300

    config - ajustes em navigationConfig

--- src/config/navigationConfig.js
+++ src/config/navigationConfig.js
@@ -1,46 +1,53 @@
+import { loginRoute } from 'modules/auth/routes'
 import { indexRoute as categoryIndexRoute } from 'modules/categories/routes'
 import { indexRoute as counterIndexRoute } from 'modules/counter/routes'
 import { indexRoute as dashboardIndexRoute } from 'modules/dashboard/routes'
 import { indexRoute as examplesIndexRoute } from 'modules/examples/routes'
 import { indexRoute as productsIndexRoute } from 'modules/products/routes'
-import { loginRoute } from 'modules/auth/routes'
+import Roles from 'services/auth/Roles'
 
 const navigationConfig = [
   {
     id: 'dashboard',
     icon: 'dashboard',
     text: 'Dashboard',
-    url: dashboardIndexRoute()
+    url: dashboardIndexRoute(),
+    role: null
   },
   {
     id: 'categories',
     icon: 'turned_in',
     text: 'Categories',
-    url: categoryIndexRoute()
+    url: categoryIndexRoute(),
+    role: Roles.DEFAULT
   },
   {
     id: 'products',
     icon: 'photo_library',
     text: 'Products',
-    url: productsIndexRoute()
+    url: productsIndexRoute(),
+    role: [Roles.DEFAULT]
   },
   {
     id: 'integrations',
     icon: 'layers',
     text: 'Examples & Tests',
-    url: examplesIndexRoute()
+    url: examplesIndexRoute(),
+    auth: true
   },
   {
     id: 'counter',
     icon: 'hourglass_empty',
     text: 'Counter App',
-    url: counterIndexRoute()
+    url: counterIndexRoute(),
+    auth: false
   },
   {
     id: 'login',
     icon: 'login',
     text: 'Login',
-    url: loginRoute()
+    url: loginRoute(),
+    auth: false
   }
 ]
 
