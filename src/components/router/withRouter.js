034243a14f754f79eba3f0ff91e31af01831b63f
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-03-01 20:39:01 -0300

    router - Criação de withRouter que foi retirado na versão 6

---
+++ src/components/router/withRouter.js
@@ -0,0 +1,17 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+
+// link: https://github.com/remix-run/react-router/issues/7156
+// link: https://gist.github.com/sibelius/7d760fa16a121ec0f4cd8b143a59bee6
+
+const withRouter = Component => {
+  const Wrapper = props => {
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    return <Component {...props} navigate={navigate} location={location} />
+  }
+
+  return Wrapper
+}
+
+export default withRouter
