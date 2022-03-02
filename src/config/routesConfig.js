8b837e8947c47234293206ca25b8f0a17c354511
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-03-02 10:53:40 -0300

    config - Ajustes nas rotas

--- src/config/routesConfig.js
+++ src/config/routesConfig.js
@@ -1,26 +1,14 @@
 import { indexRoute as dashboardIndexRoute } from 'modules/dashboard/routes'
 import { error404Route } from 'modules/errors/routes'
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { generateRoutesFromModuleConfig } from 'utils/route'
+import { createRedirectRoute, generateRoutesFromModuleConfig } from 'utils/route'
 import modulesConfig from './modulesConfig'
 
 const modulesRoutes = generateRoutesFromModuleConfig(modulesConfig)
 
 const routesConfig = [
-  {
-    path: '/',
-    element: <Navigate to={dashboardIndexRoute()} />,
-    auth: true,
-    role: []
-  },
+  createRedirectRoute('/', dashboardIndexRoute()),
   ...modulesRoutes,
-  {
-    path: '*',
-    element: <Navigate to={error404Route()} />,
-    auth: true,
-    role: []
-  }
+  createRedirectRoute('*', error404Route())
 ]
 
 export default routesConfig
