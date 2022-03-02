99dabae539044dca0897949c79347ee5a8a1b9fb
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-03-02 16:51:19 -0300

    utils - melhorias

--- src/utils/security.js
+++ src/utils/security.js
@@ -1,4 +1,4 @@
-import { isArray, isString } from 'utils'
+import { isArray, isBoolean, isString } from 'utils'
 import _ from 'lodash'
 
 export const createArrayRoles = role => {
@@ -12,10 +12,34 @@ export const createArrayRoles = role => {
   return roles
 }
 
+// Verifiar somente autorização
 export const hasPermission = (allowedRoles, userRoles = []) => {
   if (createArrayRoles(allowedRoles).some(el => userRoles.includes(el))) {
     return true
   }
 
   return false
+}
+
+// Verificar autenticação e autorização
+export const hasAuth = (itemAuth = true, itemRole = [], userIsAuthenticated = false, userRoles = []) => {
+  var _itemAuth = isBoolean(itemAuth) ? itemAuth : true
+  var _itemRole = createArrayRoles(itemRole)
+
+  // Item não precisa de autenticação
+  if (!_itemAuth) {
+    return true
+  }
+
+  // Precisa de autenticação e não está autenticado
+  if (_itemAuth && !userIsAuthenticated) {
+    return false
+  }
+
+  // Item não precisa de permissões
+  if (_itemRole.length === 0) {
+    return true
+  }
+
+  return hasPermission(_itemRole, userRoles)
 }
